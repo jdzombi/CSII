b21371b5ce66898c867b1fed032cc5b2963914ed
Jonathan Devlin <35181993+jdzombi@users.noreply.github.com>
2019-03-05 15:30:25 -0500

    intial files

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			
+			Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("/fxml/JavaFX.fxml"));
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Game");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
