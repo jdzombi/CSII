36521c9eb09fda87dfab5b5d4495bf7ed83a8d97
mistletoad <48259942+mistletoad@users.noreply.github.com>
2019-03-05 16:04:08 -0500

    btz

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
