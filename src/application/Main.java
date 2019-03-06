4ff49e9916c563c0fd5ea2538f8ae0ea7bdcf63e
Jonathan Devlin <35181993+jdzombi@users.noreply.github.com>
2019-03-06 00:04:29 -0500

    uwu

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 	
+import java.util.ResourceBundle;
+import java.net.URL;
+import javafx.fxml.FXML;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -7,25 +10,41 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.TextArea;
 
 public class Main extends Application {
+	@FXML
+	protected TextArea txtArea;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		try {
 			
 			Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("JavaFX.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Game");
 			primaryStage.show();
+			
+			ResourceBundle resources;
+			
+			txtArea.setText("gey");
+
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
+	
 
 	
 	public static void main(String[] args) {
 		launch(args);
+		
+		
+		
 	}
 }
