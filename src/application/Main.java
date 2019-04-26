c4aef9fbd74f75f80041ab2bccf75b723dd16711
Jonathan Devlin <35181993+jdzombi@users.noreply.github.com>
2019-04-26 05:53:33 -0400

    new icon!!

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
@@ -15,6 +16,7 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("JavaFX.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("file:icon.png"));
 			primaryStage.setTitle("Adventures of Jack Danger");
 			//primaryStage.setResizable(false);
 			primaryStage.show();		
